4e8c8d89f1902d3d46eeebc02fec230d7b5b476f
TinuPrajapati <tinup2580@gmail.com>
2024-09-18 20:57:18 +0530

    change

--- Frontend/src/Components/Show.jsx
+++ Frontend/src/Components/Show.jsx
@@ -8,14 +8,9 @@ function Show() {
     try {
       const getData = async () => {
         const feedback = await axios.get(
-          `${import.meta.env.VITE_API_Backend_URL}/feedback/show`,
-          {},
-          {
-            withCredentials: true,
-          }
+          `${import.meta.env.VITE_API_Backend_URL}/feedback/show`
         );
         setData(feedback.data);
-        console.log(feedback.data);
       };
       getData();
     } catch (err) {
@@ -27,7 +22,7 @@ function Show() {
     <div className="w-full h-full flex flex-col items-center gap-4 px-6 py-2">
       <h1 className="text-white text-3xl font-serif border-b-2">Show Data</h1>
       <div className="w-[100%] h-[100%] grid grid-cols-4 grid-rows-4 gap-4 rounded-md bg-white shadow-2xl p-4">
-        {data.map((item, index) => (
+        {data.map((item) => (
           <div key={item._id} className="border-2 p-2 rounded-md">
             <p>Name: {item.name}</p>
             <p>Email: {item.email}</p>
